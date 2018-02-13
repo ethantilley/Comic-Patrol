e17fd6bebb5f287ebfbc891e8e977e54bed01673
Ethan Tilley <1008464@student.sae.edu.au>
2018-02-13 17:28:43 +1000

    Player movement exept jumping. Player shooting  and bullet movment and bullet lifetime complete exept fire rate. moved in place holder art.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float baseSpeed = 5;
+    public float currentSpeed = 5;
+    public float changeSpeedPercent = 0.5f;
+    public float jumpSpeed = 2;
+    public bool jumping = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ConsistentMove();
+        CheckInput();
+	}
+
+    void ConsistentMove()
+    {
+       transform.Translate((Vector3.right * currentSpeed) * Time.deltaTime);
+    }
+
+    void PlayerJump()
+    {
+        transform.Translate((Vector3.up * jumpSpeed) * Time.deltaTime);
+    }
+
+    void CheckInput()
+    {
+        //Hack, This Jump is not at-all what i want.... 
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jumping = true;
+            PlayerJump();
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+            jumping = false;
+
+        if (jumping)
+            return;
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                currentSpeed -= (baseSpeed * changeSpeedPercent);
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+                currentSpeed = baseSpeed;
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                currentSpeed += (baseSpeed * changeSpeedPercent);
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+                currentSpeed = baseSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject bullet;
+
+    public Transform[] shootPositions;
+
+    public List<GameObject> bullets = new List<GameObject>();
+
+    public float bulletSpeed;
+    public float bulletLifeTime = 2;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckShooting();
+        MoveBullets();
+	}
+
+    void MoveBullets()
+    {
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            if (bullets[i] != null)
+            {
+                bullets[i].transform.parent = transform;
+                bullets[i].transform.Translate((Vector3.right * bulletSpeed) * Time.deltaTime);
+            }
+        }
+    }
+
+    void CheckShooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        for (int i = 0; i < shootPositions.Length; i++)
+        {
+            GameObject newBull = Instantiate(bullet, shootPositions[i].transform.position, shootPositions[i].transform.rotation);
+            Destroy(newBull, bulletLifeTime);
+            bullets.Add(newBull);
+            
+        }
+    }
+}
